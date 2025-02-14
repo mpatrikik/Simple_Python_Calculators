bc0ad1e8e47c8d687804c62f0ea62179b907a005
mpatrikik <m.patrik01@gmail.com>
2025-02-14 13:20:15 +0100

    base done

--- GUICalculator/calc.py
+++ GUICalculator/calc.py
@@ -1,8 +1,10 @@
 import tkinter as tk
 
+LARGE_FONT_STYLE = ("Arial", 40, "bold")
 SMALL_FONT_STYLE = ("Arial", 16)
 LIGHT_GRAY = "#F5F5F5"
 LABEL_COLOR = "#25265E"
+
 
 class Calculator:
     def __init__(self):
@@ -15,11 +17,18 @@ class Calculator:
         self.total_expression = "0"
         self.current_expression = "0"
         self.display_frame = self.create_display_frame()
+
+        self.total_label, self.label = self.create_display_labels()
         self.buttons_frame = self.create_display_frame()
 
     def create_display_labels(self):
         total_label = tk.Label(self.display_frame, text=self.total_expression, anchor=tk.E, bg=LIGHT_GRAY, fg=LABEL_COLOR, padx=24, font=SMALL_FONT_STYLE)
-        total_label.pack(expand=True, fill="both")
+        total_label.pack(expand=True, fill='both')
+
+        label = tk.Label(self.display_frame, text=self.current_expression, anchor=tk.E, bg=LIGHT_GRAY, fg=LABEL_COLOR, padx=24, font=LARGE_FONT_STYLE)
+        label.pack(expand=True, fill='both')
+
+        return total_label, label
 
     # centering the window
     def center_window(self):
